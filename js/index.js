a485d563052c033e0b530340269035be83c3d47c
bzr-lipe <bzr.lipe@gmail.com>
2022-11-25 13:24:36 -0300

    Terminando nav da primeira página

---
+++ js/index.js
@@ -0,0 +1,9 @@
+const aTag = document.querySelector('.logos-social-medias');
+const logo = document.querySelector('logo-social');
+
+function hoverIcon(){
+
+    
+
+    section.style.background = bg;
+}
